1fcd3b8dca684513ae117bde5a7d7506d84d112b
bznn <1018599213@qq.com>
2021-03-08 21:03:12 +0800

    add 3 lab1 files

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,19 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (x < 10) {
+            System.out.print(sum + " ");
+            x = x + 1;
+            sum = sum + x;
+        }
+        System.out.println();
+        System.out.println(5 + "10");
+    }
+}
+// coments
+
+/* block comments
+haha
+haha
+*/
